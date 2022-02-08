edef85357422df88878f388c4c641a2b3dfb25fc
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-02-08 10:02:50 -0600

    LINT: Lint for new black

--- openff/units/utilities.py
+++ openff/units/utilities.py
@@ -1,5 +1,6 @@
 from openff.utilities import get_data_file_path
 
+
 def get_defaults_path() -> str:
     """Get the full path to the defaults.txt file"""
     return get_data_file_path("data/defaults.txt", "openff.units")
