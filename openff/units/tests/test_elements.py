17376818c5eb877678998f11883e4f23d1a592f7
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-03-31 10:25:54 -0500

    MAINT: Update CI workflow, verify when OpenMM is not installed

--- openff/units/tests/test_elements.py
+++ openff/units/tests/test_elements.py
@@ -1,18 +1,18 @@
 import pytest
 from openff.utilities.testing import skip_if_missing
-from openmm.app.element import Element
 
 from openff.units.elements import MASSES, SYMBOLS
 
 
 @skip_if_missing("openmm.unit")
 @pytest.mark.parametrize(
-    "atomic_number,openmm_element,",
-    [
-        (atomic_number, Element.getByAtomicNumber(atomic_number))
-        for atomic_number in [1, 2]
-    ],
+    "atomic_number",
+    [1, 2],
 )
-def test_openmm_parity(atomic_number, openmm_element):
+def test_openmm_parity(atomic_number):
+    from openmm.app.element import Element
+
+    openmm_element = Element.getByAtomicNumber(atomic_number)
+
     assert MASSES[atomic_number].m == openmm_element.mass._value
     assert SYMBOLS[atomic_number] == openmm_element.symbol
