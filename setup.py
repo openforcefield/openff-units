b8fdc8bcd723083bcc639b34a5d3f4f6a57d4beb
Matt Thompson <mattwthompson@protonmail.com>
2024-03-25 12:05:48 -0500

    Test with Pint 0.23, add downstream testing (#83)
    * Update linters
    
    * Test with Pint 0.23
    
    * Add downstream package for testing
    
    * Support Python 3.9
    
    * Upload to CodeCov with token

--- setup.py
+++ setup.py
@@ -2,33 +2,32 @@
 openff-toolkit
 A common units module for the OpenFF software stack
 """
+
 import sys
-from setuptools import setup, find_namespace_packages
+
+from setuptools import find_namespace_packages, setup
+
 import versioneer
 
 short_description = __doc__.split("\n")
 
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
-try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = "\n".join(short_description[2:])
+long_description = open("README.md", "r").read()
 
 
 setup(
-    name='openff-units',
-    author='Open Force Field Initiative',
-    author_email='info@openforcefield.org',
+    name="openff-units",
+    author="Open Force Field Initiative",
+    author_email="info@openforcefield.org",
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='MIT',
-    packages=find_namespace_packages(include=['openff.*']),
+    license="MIT",
+    packages=find_namespace_packages(include=["openff.*"]),
     package_data={"openff.units": ["py.typed"]},
     include_package_data=True,
     setup_requires=[] + pytest_runner,
