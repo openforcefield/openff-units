29acaa96c31df0377d73f0f18a4a7340fb6ea38d
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-06-17 08:46:01 -0500

    Test basic behvaior of each supported input

--- openff/units/_tests/test_pydantic.py
+++ openff/units/_tests/test_pydantic.py
@@ -1,6 +1,7 @@
+import pytest
 from pydantic import BaseModel
 
-from openff.units import Quantity
+from openff.units import Quantity, unit
 
 
 def test_model_definition():
@@ -8,3 +9,22 @@ def test_model_definition():
 
     class MyModel(BaseModel):
         x: Quantity
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        1.0 * unit.angstrom,
+        Quantity("1.0 angstrom"),
+        "1.0 angstrom",
+        {"magnitude": 1.0, "units": "angstrom"},
+    ],
+)
+def test_basic_field_validation(value):
+    class MyModel(BaseModel):
+        x: Quantity
+
+    stored_value = MyModel(x=value).x
+
+    assert str(stored_value.units) == "angstrom"
+    assert stored_value.m_as("angstrom") == 1.0
