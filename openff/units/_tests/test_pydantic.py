db18d2a3aefd345fb4d69ed6055f55c0e218bca6
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-06-17 09:25:14 -0500

    Improve testing of iterable inputs

--- openff/units/_tests/test_pydantic.py
+++ openff/units/_tests/test_pydantic.py
@@ -59,3 +59,38 @@ def test_model_roundtrip(serialize_with):
     assert type(new_model.y.m) is not list
     assert type(new_model.y.m) is numpy.ndarray
     assert new_model.z == Quantity(299.99, unit.kelvin)
+
+
+@pytest.mark.parametrize("serialize_with", ["python", "json"])
+def test_different_iterables(serialize_with):
+    class MyModel(BaseModel):
+        a: Quantity
+        b: Quantity
+        c: Quantity
+
+    model = MyModel(
+        a=(300.0, 300.1) * unit.kelvin,
+        b=[12.011, 1.008, 1.008, 0.0] * unit.amu,
+        c=4.0 * numpy.eye(3) * unit.angstrom,
+    )
+
+    match serialize_with:
+        case "json":
+            model_as_json = model.model_dump_json()
+            new_model = MyModel.model_validate_json(model_as_json)
+        case "python":
+            model_as_dict = model.model_dump()
+            new_model = MyModel.model_validate(model_as_dict)
+
+    assert new_model.a.shape == (2,)
+    assert new_model.b.shape == (4,)
+    assert new_model.c.shape == (3, 3)
+
+    assert str(new_model.a.units) == "kelvin"
+    assert str(new_model.b.units) == "unified_atomic_mass_unit"
+    assert str(new_model.c.units) == "angstrom"
+
+    assert new_model.a[1].m == 300.1
+    assert new_model.b[0].m == 12.011
+    assert new_model.b[-1].m == 0.0
+    assert new_model.c[2, 2].m == 4.0
